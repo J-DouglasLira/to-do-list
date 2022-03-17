fb948e24a33639be173a647d303c669b8c2f5036
J-DouglasLira <jdouglira@gmail.com>
2022-03-17 15:02:41 -0300

    muio materials consumed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import './styles/Global.scss';
+import React from "react";
+import "./styles/Global.scss";
+import Forms from "./components/Forms";
+import TodoItem from "./components/TodoItem";
 
-export function App(){
-  return <h1>Hello World!</h1>
+export function App() {
+  return (
+    <div className="App">
+      <Forms />
+      <TodoItem />
+    </div>
+  );
 }
-
-
-
-
